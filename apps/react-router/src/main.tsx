f47998e8b5667b435c0c9d726f0065f841be7c50
ofirrifo <ofirrifo@gmail.com>
2024-02-02 11:32:18 +0200

    first commit

--- apps/react-router/src/main.tsx
+++ apps/react-router/src/main.tsx
@@ -1,13 +1,34 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './app/Root';
+import ErrorPage from './app/ErrorPage';
+import Contact from './app/Contact';
 
-import App from './app/app';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
+    
   </StrictMode>
 );
